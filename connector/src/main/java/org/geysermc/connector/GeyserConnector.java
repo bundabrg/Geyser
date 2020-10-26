42173d7addab819275076e5e957580c7371d42ee
bundabrg <bundabrg@grieve.com.au>
2020-10-27 07:14:42 +0800

    Remove duplicate initialization

--- connector/src/main/java/org/geysermc/connector/GeyserConnector.java
+++ connector/src/main/java/org/geysermc/connector/GeyserConnector.java
@@ -86,7 +86,7 @@ public class GeyserConnector {
             .enable(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES);
 
     public static final String NAME = "Geyser";
-    public static final String VERSION = "DEV"; // A fallback for running in IDEs
+    public static final String VERSION = "1.2.0-SNAPSHOT (git-bleeding-ef4cc34)"; // A fallback for running in IDEs
 
     private static final String IP_REGEX = "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b";
 
@@ -142,7 +142,6 @@ public class GeyserConnector {
 
         /* Initialize translators and registries */
         BiomeTranslator.init();
-        BlockTranslator.init();
         BlockEntityTranslator.init();
         EffectRegistry.init();
         EntityIdentifierRegistry.init();
